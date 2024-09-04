90af1683de4335e57680977e4ee7374fc841809f
JMSBPP <jmsbpp@hotmail.es>
2024-09-03 22:29:59 -0500

    corrected contract logic

--- hashtables/1282/contracts/src/stateContract.sol
+++ hashtables/1282/contracts/src/stateContract.sol
@@ -1,100 +1,131 @@
 pragma solidity ^0.8.0;
 
-
-
 contract StateContract {
-    
     uint size;
     uint person;
     uint currentSize;
+    uint groupID;
 
     bool lastPerson;
-    bytes[] groups;
-    uint[] lastSize;
+    uint[][] groups;
+    uint lastSize;
 
     enum state {
         INITIAL,
         PROGRESS,
+        MEMBERPROCESSED,
         DONE
     }
 
     state _state;
 
-    modifier groupFilled{
-        require(currentSize==0, 'NOT FILLED YET'),
+    modifier groupFilled() {
+        require(currentSize == 0, "NOT FILLED YET");
         _;
     }
 
-    modifier otherGroup{
-        require(size< lastSize[0], 'PERSON DOES NOT FIT IN GROUP'),
+    modifier otherGroup() {
+        require(size < lastSize, "PERSON DOES NOT FIT IN GROUP");
         _;
     }
 
-    modifier receivingMembers{
-        require(size==lastSize[0], 'NOT VALID STATE')
-    }
-
-    modifier starting{
-        require(_state == state.INITIAL, "StateContract: Already initialized"),
+    modifier receivingMembers() {
+        require(size == lastSize, "NOT VALID STATE");
         _;
     }
 
-    modifier working{
-        require(_state == state.PROGRESS, "StateContract: Already initialized"),
+    modifier starting() {
+        require(_state == state.INITIAL, "StateContract: Already initialized");
         _;
     }
 
-    modifier lastPersonCall{
-        require(lastPerson==true, 'NOT LAST PERSON'), 
+    modifier working() {
+        require(_state == state.PROGRESS, "StateContract: Already initialized");
         _;
     }
-    
 
-    function initialize(uint person, uint _size bool _lastPerson) public starting{
+    modifier lastPersonCall() {
+        require(lastPerson == true, "NOT LAST PERSON");
+        _;
+    }
 
+    modifier processing() {
+        require(_state == state.MEMBERPROCESSED, "NOT VALID STATE");
+        _;
+    }
+
+    event MemberAdded(uint indexed _person, uint indexed _group);
+    event GroupFilled(uint indexed _group);
+
+    function initialize(
+        uint _person,
+        uint _size,
+        bool _lastPerson
+    ) public starting {
         size = _size;
         person = _person;
         lastPerson = _lastPerson;
-        lastSize.append(-1);
+        lastSize = _size + 1;
         currentSize = size;
-        state = state.PROGRESS;
+        groupID = 1;
+        _state = state.PROGRESS;
+        processor();
+    }
+
+    function work(
+        uint _person,
+        uint _size,
+        bool _lastPerson
+    ) public processing {
+        size = _size;
+        person = _person;
+        lastPerson = _lastPerson;
+        _state = state.PROGRESS;
+        processor();
     }
 
     function removeLastSize() internal groupFilled working {
-        lastSize.pop();
+        lastSize = size + 1;
     }
 
-    function createGroup(uint _size) internal otherGroup working {
-        uint[_size] group;
-        groups.append(group);
-        assert groups[0] == group;
+    function createGroup() internal otherGroup working {
+        uint[] memory group;
+        groups.push(group);
     }
 
-    function updateSize(uint _size) internal otherGroup working{
-        lastSize.push(_size);
+    function updateSize(uint _size) internal otherGroup working {
+        lastSize = _size;
     }
 
-    function addMember() internal receivingMembers working{
+    function addMember() internal receivingMembers working {
         groups[0].push(person);
-        currentSize -=1;
+        currentSize -= 1;
     }
 
-    function sendGroups() external lastPersonCall working{
-    //
+    function processor() private working {
+        if (currentSize == 0) {
+            removeLastSize();
+            emit GroupFilled(groupID);
+            groupID += 1;
+            groups.pop();
+            processor();
+        }
+        if (size < lastSize && currentSize != 0) {
+            updateSize(size);
+            currentSize = size;
+            createGroup();
+            processor();
+        }
+        if (size == lastSize && currentSize != 0) {
+            addMember();
+            currentSize -= 1;
+            emit MemberAdded(person, groupID);
+        }
+    }
+
+    function sendGroups() external lastPersonCall working {
+        //
 
         _state = state.DONE;
     }
-
-
-
-}
-
-
-
-    
-
-
-
-
-
 }
