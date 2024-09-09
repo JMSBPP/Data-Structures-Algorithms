ff858bb7184ba2787d7b635199f935392ae8b3d3
JMSBPP <jmsbpp@hotmail.es>
2024-09-09 13:06:17 -0500

    testing contracts

--- hashtables/2325/contracts/src/solution.sol
+++ hashtables/2325/contracts/src/solution.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.26;
 import "../dependencies/reactive-smart-contract-demos/src/IReactive.sol";
 import "../dependencies/reactive-smart-contract-demos/src/AbstractReactive.sol";
 import "../dependencies/reactive-smart-contract-demos/src/ISystemContract.sol";
@@ -9,17 +9,17 @@ interface IClient {
     function sendKey(string memory key) external;
 }
 contract Client {
-    event MessageSent(bytes encodedKey, bytes encodedMessage);
+    event MessageRequest(string keys, string message);
 
     function sendKey(string memory key, string memory message) public {
-        emit MessageSent(abi.encodePacked(key), abi.encodePacked(message));
+        emit MessageRequest(key, message);
     }
 }
 
 // is AbstractCallback
 contract Server {
     IClient public client;
-    mapping(string => string) private cipherMap;
+    mapping(string => string) public cipherMap;
 
     event Response(string message);
     // constructor(
@@ -30,57 +30,74 @@ contract Server {
     // }
 
     receive() external payable {}
-    function strHandler(
+    function alreadyAdded(
+        bytes1 _byte,
+        bytes memory byteArray
+    ) internal pure returns (bool) {
+        for (uint256 i = 0; i < byteArray.length; i++) {
+            if (byteArray[i] == _byte) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function TrimstrHandler(
         string memory str
-    ) internal pure returns (string[] memory) {
+    ) public pure returns (string[] memory) {
         bytes memory b = bytes(str);
         string[] memory res = new string[](b.length);
         for (uint256 i = 0; i < b.length; i++) {
             bytes memory b1 = new bytes(1);
             b1[0] = b[i];
-            res[i] = string(b1);
+            if (b1[0] != 0x20 && !alreadyAdded(b1[0], b)) {
+                res[i] = string(b1);
+            }
         }
 
         return res;
     }
 
-    function cipherGen() internal pure returns (string[] memory) {
-        return strHandler("abcdefghijklmnopqrstuvwxyz");
-    }
+    //     function cipherGen() internal pure returns (string[] memory) {
+    //         return TrimstrHandler("abcdefghijklmnopqrstuvwxyz");
+    //     }
 
-    function getKeyMappings(bytes memory encodedKey) internal {
-        string memory _key = abi.decode(encodedKey, (string));
-        string[] memory key = strHandler(_key);
-        string[] memory cipher = cipherGen();
+    //     function getKeyMappings(string memory _key) public {
+    //         string[] memory key = TrimstrHandler(_key);
+    //         string[] memory cipher = cipherGen();
+    //         uint size = key.length >= cipher.length ? cipher.length : key.length;
+    //         cipherMap[" "] = " ";
+    //         for (uint256 i = 0; i < size; i++) {
+    //             cipherMap[key[i]] = cipher[i];
+    //         }
+    //     }
 
-        for (uint256 i = 0; i < key.length; i++) {
-            cipherMap[key[i]] = cipher[i];
-        }
-    }
+    //     function getKeyMapping(
+    //         string memory key
+    //     ) public view returns (string memory) {
+    //         return cipherMap[key];
+    //     }
 
-    function revealMessage(
-        bytes memory encodedKeys,
-        bytes memory _message_
-    ) public {
-        string memory message = abi.decode(_message_, (string));
-        getKeyMappings(encodedKeys);
-        string[] memory _message = strHandler(message);
-        bytes memory concatenatedBytes = new bytes(_message.length);
-        uint currentIndex = 0;
-        // Append each string to the concatenatedBytes array
-        for (uint i = 0; i < _message.length; i++) {
-            bytes memory strBytes = bytes(cipherMap[_message[i]]);
-            for (uint j = 0; j < strBytes.length; j++) {
-                concatenatedBytes[currentIndex] = strBytes[j];
-                currentIndex++;
-            }
-        }
-        emit Response(string(concatenatedBytes));
-    }
+    //     function revealMessage(string memory keys, string memory _message) public {
+    //         string[] memory message = TrimstrHandler(_message);
+    //         getKeyMappings(keys);
+    //         bytes memory concatenatedBytes = new bytes(message.length);
+    //         uint currentIndex = 0;
+
+    //         // Append each string to the concatenatedBytes array
+    //         for (uint i = 0; i < message.length; i++) {
+    //             bytes memory strBytes = bytes(cipherMap[message[i]]);
+    //             for (uint j = 0; j < strBytes.length; j++) {
+    //                 concatenatedBytes[currentIndex] = strBytes[j];
+    //                 currentIndex++;
+    //             }
+    //         }
+    //         emit Response(string(concatenatedBytes));
+    //     }
+    // }
+
+    // contract API is IReactive, AbstractReactive {
+    //     uint256 private constant SEPOLIA_CHAIN_ID = 11155111;
+
+    //     receive() external payable {}
+    // }
 }
-
-// contract API is IReactive, AbstractReactive {
-//     uint256 private constant SEPOLIA_CHAIN_ID = 11155111;
-
-//     receive() external payable {}
-// }
--- hashtables/2325/contracts/test/solution.t.sol
+++ hashtables/2325/contracts/test/solution.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import {Test} from "../lib/forge-std/src/Test.sol";
 import {Server, Client} from "../src/solution.sol";
-
+import "../lib/forge-std/src/console.sol";
 contract SolutionTest is Test {
     Server server;
     Client client;
@@ -20,11 +20,30 @@ contract SolutionTest is Test {
         );
     }
 
-    function testServerMessage() external {
-        bytes memory encodedMessage = abi.encodePacked("vkbs bs t suepuv");
-        bytes memory encodedKey = abi.encodePacked(
-            "the quick brown fox jumps over the lazy dog"
-        );
-        server.revealMessage(encodedKey, encodedMessage);
+    function testTrimstrHandler() external {
+        server.TrimstrHandler("tta");
     }
+
+    // function testGetKeyMappings() external {
+    //     string memory keys = "the quick brown fox jumps over the lazy dog";
+    //     server.getKeyMappings(keys);
+    //     server.getKeyMapping("t");
+    //     server.getKeyMapping("h");
+    //     server.getKeyMapping("e");
+    //     server.getKeyMapping(" ");
+    //     server.getKeyMapping("q");
+    //     server.getKeyMapping("u");
+    //     server.getKeyMapping("i");
+    //     server.getKeyMapping("c");
+    //     server.getKeyMapping("k");
+    //     server.getKeyMapping(" ");
+    //     server.getKeyMapping("b");
+    //     server.getKeyMapping("r");
+    // }
+
+    // function testServerMessage() external {
+    //     string memory message = "vkbs bs t suepuv";
+    //     string memory keys = "the quick brown fox jumps over the lazy dog";
+    //     server.revealMessage(keys, message);
+    // }
 }
